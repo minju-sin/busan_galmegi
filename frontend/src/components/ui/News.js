a08b44f603791e9b1c1259a6d1bd51a16a6a194f
minju-sin <sinminju04@gmail.com>
2024-02-13 22:07:55 +0900

    메인 페이지 디자인 기초 완성
    sns 저장

--- frontend/src/components/ui/News.js
+++ frontend/src/components/ui/News.js
@@ -2,14 +2,37 @@
 
 import React from "react";
 import { styled } from 'styled-components';
+import { StyledTitle } from "./Sns";
 
+const StyledNewsWrapper = styled.div`
+    display: flex;
+    width: 100%; /* 너비를 100%로 설정하여 화면에 맞춤 */
+    max-width: 1080px;
+    height: 270px;
+    margin: 0 auto; /* 좌우 여백을 자동으로 설정하여 가운데 정렬 */
+    align-items: center;
+    border-radius: 10px;
+`;
 
+const StyledNewsImg = styled.img`
+    flex-direction: row;
+    width: 295px;
+    height: 270px;
+    flex-shrink: 0;
+    margin: 0 auto; /* 좌우 여백을 자동으로 설정하여 가운데 정렬 */
+    align-items: center;
+`;
 
 
 const News = ({ }) => {
     return (
         <>
-        
+        <StyledTitle>뉴스</StyledTitle>
+        <StyledNewsWrapper>
+            <StyledNewsImg alt="자이언츠 뉴스1"/>
+            <StyledNewsImg alt="자이언츠 뉴스2"/>
+            <StyledNewsImg alt="자이언츠 뉴스3"/>
+        </StyledNewsWrapper>
         </>
     );
 };
--- frontend/src/components/ui/Sns.js
+++ frontend/src/components/ui/Sns.js
@@ -1,23 +1,60 @@
-// ./MainHeader.js
-
 import React from "react";
 import { styled } from 'styled-components';
+import Youtube  from '../images/Youtube.jpg';
+import Instagram  from '../images/Instagram.jpg';
+import Facebook  from '../images/Facebook.jpg';
 
-const StyledFooterWrapper = styled.div`
-    width: 100%; /* 너비를 100%로 설정하여 화면에 맞춤 */
-    height: 350px;
-    margin: 0 auto; /* 좌우 여백을 자동으로 설정하여 가운데 정렬 */
-    align-items: center;
-    background-color: #EEEEEE;
+export const StyledTitle = styled.p`
+    width: 100%;
+    max-width: 1080px;
+    margin: 0 auto;
+    color: #000;
+    font-family: "Bold";
+    font-size: 40px;
 `;
 
+const StyledSNSWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    max-width: 1080px;
+    height: 120px;
+    margin: 0 auto;
+    align-items: center;
+`;
 
+const StyledSNSImgLink = styled.a`
+    flex-direction: row;
+    width: 295px;
+    height: 120px;
+    flex-shrink: 0;
+    margin: 0 auto;
+    align-items: center;
+    border-radius: 10px;
+    cursor: pointer; /* 마우스 포인터를 손가락 모양으로 변경하여 클릭 가능함을 나타냄 */
+`;
 
+const StyledSNSImg = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover; /* 이미지가 div 요소에 꽉 차게 */
+    border-radius: 10px;
+`;
 
-const Sns = ({ }) => {
+const Sns = () => {
     return (
         <>
-        <p>sns</p>
+        <StyledTitle>SNS</StyledTitle>
+        <StyledSNSWrapper>
+            <StyledSNSImgLink href="https://www.youtube.com/channel/UCAZQZdSY5_YrziMPqXi-Zfw">
+                <StyledSNSImg src={Youtube} alt="자이언츠 유튜브" />
+            </StyledSNSImgLink>
+            <StyledSNSImgLink href="https://www.instagram.com/busanlottegiants/">
+                <StyledSNSImg src={Instagram} alt="자이언츠 인스타" />
+            </StyledSNSImgLink>
+            <StyledSNSImgLink href="https://www.facebook.com/lottegiantsbusan/">
+                <StyledSNSImg src={Facebook} alt="자이언츠 페이스북" />
+            </StyledSNSImgLink>
+        </StyledSNSWrapper>
         </>
     );
 };
