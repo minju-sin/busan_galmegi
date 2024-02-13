0a9c704859faaee5abc4db346ef020800bb14320
minju-sin <sinminju04@gmail.com>
2024-02-13 16:29:36 +0900

    프론트 의존성 추가

---
+++ frontend/src/components/page/MainPage.js
@@ -0,0 +1,16 @@
+// ./MainPage.js 
+// src/components/Home.js
+
+import React from "react";
+import MainHeader from "../ui/MainHeader";
+
+function MainPage() {
+
+    return (
+        <>
+            <MainHeader/>
+        </>
+    );
+}
+
+export default MainPage;
---
+++ frontend/src/components/ui/MainHeader.js
@@ -0,0 +1,13 @@
+// ./MainHeader.js
+
+import React from "react";
+
+const MainHeader = ({ }) => {
+    return (
+        <div>
+            <p>부산갈매기</p>
+        </div>
+    );
+};
+
+export default MainHeader;
