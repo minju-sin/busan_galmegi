11aa4760559dbecee68849b30b55549f05ed4a76
minju-sin <sinminju04@gmail.com>
2024-02-13 17:39:40 +0900

    헤더 만들기

--- frontend/src/components/ui/MainHeader.js
+++ frontend/src/components/ui/MainHeader.js
@@ -1,12 +1,77 @@
 // ./MainHeader.js
 
 import React from "react";
+import { styled } from 'styled-components';
+import logoImage from '../images/Logo.svg';
+import profileImage from '../images/profile.svg';
+
+
+const StyledMainHeader = styled.div`
+    display: flex;
+    width: 100%; /* 너비를 100%로 설정하여 화면에 맞춤 */
+    max-width: 1440px; /* 최대 너비를 1440px로 제한 */
+    height: 127px;
+    margin: 0 auto; /* 좌우 여백을 자동으로 설정하여 가운데 정렬 */
+    align-items: center;
+    flex-shrink: 0;
+    background-color: #374B73;
+`;
+
+const StyledMainLogo = styled.p`
+    width: 100%; /* 너비를 100%로 설정하여 화면에 맞춤 */
+    max-width: 1440px; /* 최대 너비를 1440px로 제한 */
+    margin: 0 auto; /* 좌우 여백을 자동으로 설정하여 가운데 정렬 */
+    width: 641px;
+    color: #FFF;
+    text-align: center;
+    font-family: "Inline";
+    font-size: 80px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+`;
+
+const StyledLogo = styled.img`
+    margin: 0 auto; /* 좌우 여백을 자동으로 설정하여 가운데 정렬 */
+    height: 80px;
+`;
+
+const StyledNavi = styled.div`
+    width: 100%; /* 너비를 100%로 설정하여 화면에 맞춤 */
+    max-width: 1440px; /* 최대 너비를 1440px로 제한 */
+    margin: 0 auto; /* 좌우 여백을 자동으로 설정하여 가운데 정렬 */
+    height: 89px;
+    flex-shrink: 0;
+    display: flex;
+    justify-content: center;
+`;
+
+const StyledNaviP = styled.p`
+    color: #000;
+    text-align: center;
+    font-family: "Bold";
+    font-size: 20px;
+    width: 105px;
+    flex-direction: row;
+`;
 
 const MainHeader = ({ }) => {
     return (
-        <div>
-            <p>부산갈매기</p>
-        </div>
+        <>
+        <StyledMainHeader>
+            <StyledLogo src={logoImage} alt="로고이미지"/>
+            <StyledMainLogo>부산갈매기</StyledMainLogo>
+            <StyledLogo src={profileImage} alt="프로필"/>
+        </StyledMainHeader>
+
+        <StyledNavi>
+            <StyledNaviP>미디어</StyledNaviP>
+            <StyledNaviP>소개</StyledNaviP>
+            <StyledNaviP>경기/중계</StyledNaviP>
+            <StyledNaviP>마!톡</StyledNaviP>
+            <StyledNaviP>티켓</StyledNaviP>
+        </StyledNavi>
+        </>
     );
 };
 
