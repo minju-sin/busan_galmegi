d0c0cebfddb397442357ef085b8bdb1314617d5c
minju-sin <sinminju04@gmail.com>
2024-02-19 20:25:45 +0900

    user 라우터 수정

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,21 +1,10 @@
 const express = require("express");
-<<<<<<< HEAD
-const { getUserProfile } = require("../controllers/userController");
-=======
-const { saveUserData, getUserProfile } = require("../controllers/userController");
->>>>>>> fc48cfdbdc4b757e267015bf96938dd87c3af47f
+const {getUserProfile} = require("../controllers/userController");
 const router = express.Router();
 
-// 회원 정보 
+// 사용자 정보
 router
-<<<<<<< HEAD
-=======
-.route("/")
-    .post(saveUserData);
-
-router
->>>>>>> fc48cfdbdc4b757e267015bf96938dd87c3af47f
 .route("/:id")
-    .get(getUserProfile)
+    .get(getUserProfile);
 
 module.exports = router;
