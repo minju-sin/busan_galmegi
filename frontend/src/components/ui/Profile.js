0f1d26e237cd399103dcf75798cdb38102e66a9a
minju-sin <sinminju04@gmail.com>
2024-02-19 15:59:12 +0900

    프로필 프론트 디자인
    mui - icons 라이브러리 설치

--- frontend/src/components/ui/Profile.js
+++ frontend/src/components/ui/Profile.js
@@ -1,17 +1,63 @@
-// ./ui/Profile.js
-
-// ./ui/NaverLogin.js
+// ../ui/Profile.js
 
 import React from 'react';
 import profileImage from '../images/profile.svg';
-import { StyledLogo } from './MainHeader';
-
+import { StyledLogo, StyledProfile } from './MainHeader';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
 
 function Profile() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <>
-            <StyledLogo src={profileImage} alt="프로필"/>
+            <StyledProfile onClick={handleClick}>
+                <StyledLogo src={profileImage} alt="프로필" />
+            </StyledProfile>
+
+            {/* Menu 컴포넌트를 사용하여 클릭한 위치에 메뉴가 표시되도록 설정 */}
+            <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                getContentAnchorEl={null}
+            >
+                {/* 메뉴 아이템 */}
+                <MenuItem onClick={handleClose}>000님</MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                        <Settings fontSize="small" />
+                    </ListItemIcon>
+                    Settings
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                        <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
         </>
     );
 }
