c6c42ff48da8b04d64d4f891b1be1beddf81dc70
minju-sin <sinminju04@gmail.com>
2024-02-21 17:19:07 +0900

    이미지 추가

--- frontend/src/components/page/NewsPage.js
+++ frontend/src/components/page/NewsPage.js
@@ -7,6 +7,7 @@ import Footer from "../ui/Footer.js";
 import { StyledTitle } from "../ui/Sns.js";
 import FetchNewsData from "../hooks/FetchNewsData.js";
 import { StyledIntro } from "../ui/Sajik.js";
+import MainImg1 from '../images/MainImg1.jpg';
 
 function NewsPage() {
     const newsData = FetchNewsData();
@@ -18,6 +19,7 @@ function NewsPage() {
                 <StyledTitle>뉴스</StyledTitle>
                     {newsData.map((newsItem, index) => (
                         <div key={index}>
+                            <img src={MainImg1} alt="뉴스" style={{width: '100px', height: '100px'}}/>
                             <a href={newsItem.link} target="_blank" rel="noopener noreferrer" dangerouslySetInnerHTML={{ __html: newsItem.title }} />
 
                             <p dangerouslySetInnerHTML={{ __html: newsItem.description }} />
