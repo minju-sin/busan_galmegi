1af12dcf2bb153cef8367bdf73ae1bd4cdd270d4
minju-sin <sinminju04@gmail.com>
2024-02-22 16:59:09 +0900

    날짜 변환 함수 모듈화

---
+++ frontend/src/components/common/formatDate.js
@@ -0,0 +1,12 @@
+// 날짜 변환 함수
+
+export function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    
+    return `${year}년 ${month}월 ${day}일 ${hours}시${minutes}분`;
+}
--- frontend/src/components/page/NewsPage.js
+++ frontend/src/components/page/NewsPage.js
@@ -6,8 +6,9 @@ import FetchNewsData from "../hooks/FetchNewsData.js";
 import { StyledIntro } from "../ui/Sajik.js";
 import MainImg1 from '../images/MainImg1.jpg';
 import styled from "styled-components";
+import { formatDate } from './../common/formatDate';
 
-const StyledNewsDiv = styled.div`
+export const StyledNewsDiv = styled.div`
     display: flex;
     margin-bottom: 10px;
     cursor: pointer;
@@ -18,39 +19,27 @@ const StyledNewsDiv = styled.div`
     }
 `;
 
-const StyledMainImg = styled.img`
+export const StyledMainImg = styled.img`
     width: 200px;
     height: 200px;
     margin-right: 20px;
     border-radius: 10px;
 `;
 
-const StyledMainTextContainer = styled.div`
+export const StyledMainTextContainer = styled.div`
     flex: 1;
 `;
 
-const StyledMainTitle = styled.p`
+export const StyledMainTitle = styled.p`
     font-family: 'Bold';
     font-size: 25px;
     margin-bottom: 30px;
 `;
 
-const StyledMainText = styled.p`
+export const StyledMainText = styled.p`
     font-family: 'Regular';
     font-size: 15px;
 `;
-
-// 날짜 변환 함수
-function formatDate(dateString) {
-    const date = new Date(dateString);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    
-    return `${year}년 ${month}월 ${day}일 ${hours}시${minutes}분`;
-}
 
 function NewsPage() {
     const newsData = FetchNewsData();
