85eea9b701f172d1c8f008f4743346115d4f2ca2
minju-sin <sinminju04@gmail.com>
2024-02-26 11:29:32 +0900

    기초 프론트단 상세 페이지 디자인 구상

--- frontend/src/components/ui/MaTalkDetail.js
+++ frontend/src/components/ui/MaTalkDetail.js
@@ -2,13 +2,45 @@
 
 
 import React from "react";
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import { StyledIntro, StyledTitle } from "../styles/Intro/intro";
 
 function MaTalkDetail() {
 
+
     return (
-        <>
-         
-        </>
+        <StyledIntro>
+            <p>이전</p>
+            <StyledTitle>제목</StyledTitle>
+            <p>내용</p>
+
+            <ButtonGroup variant="contained" aria-label="Basic button group">
+                <Button 
+                    sx={{ 
+                    backgroundColor: '#041E42', 
+                    color: 'white', 
+                    '&:hover': { backgroundColor: '#303f9f' }, fontFamily:"Regular",
+                    width: '150px',
+                    height: '50px'
+                    }}
+                >
+                    수정하기
+                </Button>
+                <Button 
+                    sx={{ 
+                    backgroundColor: '#D00F31',
+                    color: 'white', 
+                    '&:hover': { backgroundColor: '#d32f2f' },
+                    fontFamily:"Regular",
+                    width: '150px',
+                    height: '50px'
+                    }}
+                >
+                    삭제하기
+                </Button>
+            </ButtonGroup>
+        </StyledIntro>
     );
 }
 
